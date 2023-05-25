68e7eb6468d8dedad81b51e6c702d74e7793e82f
RealVlusion <vlusionwebbuilding@gmail.com>
2023-05-25 11:39:19 +0200

    refactor: move functions to index.js

--- functions/index.js
+++ functions/index.js
@@ -17,3 +17,17 @@
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+const database = admin.firestore();
+const { initializeApp } = require("firebase-admin/app");
+
+initializeApp();
+
+exports.timerUpdate = functions.pubsub.schedule('every 5 minutes').onRun((context) => {
+    database.doc("NOTIFICATIONS/91xoK9Y5Jz69Nqlhm2Ky").update({ "time": admin.firestore.Timestamp.now() });
+    console.log('Successful timer update');
+    return null;
+});
