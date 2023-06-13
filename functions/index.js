03e6a937240bdf1493edc8b51513bda0d1e11084
RealVlusion <vlusionwebbuilding@gmail.com>
2023-06-13 15:38:55 +0200

    feat: create cloud function for training evaluation reminder

--- functions/index.js
+++ functions/index.js
@@ -42,3 +42,42 @@ exports.dailyNotificationForUpcomingTrainings = functions.pubsub.schedule('every
         }
     }
 });
+
+exports.dailyNotificationForTrainingEvaluation = functions.pubsub.schedule('every 5 minutes').onRun(async (context) => {
+    const today = new Date();
+    const oneWeekBack = new Date();
+    oneWeekBack.setDate(today.getDate() - 7);
+    oneWeekBack.setHours(0, 0, 0, 0);
+
+    const trainingsSnapshot = await admin.firestore().collection("training_planning")
+        .where("endDate", ">=", oneWeekBack)
+        .where("endDate", "<", new Date(oneWeekBack.getTime() + 24 * 60 * 60 * 1000))
+        .get();
+
+    const trainings = trainingsSnapshot.docs.map((doc) => doc.data());
+
+    for (const trainingSnapshot of trainingsSnapshot.docs) {
+        const trainingData = trainingSnapshot.data();
+        const trainingId = trainingSnapshot.id;
+
+        const trainingApplicationsSnapshot = await admin.firestore().collection("training_application")
+            .where("planningId", "==", trainingId)
+            .get();
+
+        const applications = trainingApplicationsSnapshot.docs.map((doc) => doc.data());
+
+        for (const applicationSnapshot of trainingApplicationsSnapshot.docs) {
+            const applicationData = applicationSnapshot.data();
+            await admin.firestore().collection("mails").add({
+                to: [applicationData.userId],
+                template: {
+                    name: "evaluation_reminder",
+                    data: {
+                        email: applicationData.userId,
+                    },
+                },
+            });
+
+        }
+    }
+});
